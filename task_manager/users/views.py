68aa1b073c4016a647ef2bb60136cfdb8fd323a3
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-03-04 23:46:41 +0530

    fix - user register success url

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -5,5 +5,5 @@ from .forms import UserSignUpForm
 
 class SignUpView(generic.CreateView):
     form_class = UserSignUpForm
-    success_url = "/users/login"
+    success_url = "/user/login"
     template_name = "tasks/user_create.html"
